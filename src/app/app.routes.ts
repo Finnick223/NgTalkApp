5edda0204815e62d9de8e70e9de755cf114b8721
Finnick223 <filipf192@gmail.com>
2025-04-17 11:57:00 +0200

    feat: add tailwind and primeng with basic layout+pages structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [{ path: '', component: LandingPageComponent }],
+  },
+];
