b9900222e7db7141e172a22cf7acf769493e16aa
Finnick223 <filipf192@gmail.com>
2025-04-23 12:28:15 +0200

    feat: theme preset

---
+++ src/styles/theme-preset.ts
@@ -0,0 +1,50 @@
+import { definePreset } from '@primeng/themes';
+import Aura from '@primeng/themes/aura';
+
+export const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{indigo.50}',
+      100: '{indigo.100}',
+      200: '{indigo.200}',
+      300: '{indigo.300}',
+      400: '{indigo.400}',
+      500: '{indigo.500}',
+      600: '{indigo.600}',
+      700: '{indigo.700}',
+      800: '{indigo.800}',
+      900: '{indigo.900}',
+      950: '{indigo.950}',
+    },
+    surface: {
+      0: '#ffffff',
+      50: '{zinc.50}',
+      100: '{zinc.100}',
+      200: '{zinc.200}',
+      300: '{zinc.300}',
+      400: '{zinc.400}',
+      500: '{zinc.500}',
+      600: '{zinc.600}',
+      700: '{zinc.700}',
+      800: '{zinc.800}',
+      900: '{zinc.900}',
+      950: '{zinc.950}',
+    },
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{indigo.500}',
+          inverseColor: '#ffffff',
+          hoverColor: '{indigo.900}',
+          activeColor: '{indigo.800}',
+        },
+        highlight: {
+          background: '{indigo.950}',
+          focusBackground: '{indigo.700}',
+          color: '#ffffff',
+          focusColor: '#ffffff',
+        },
+      },
+    },
+  },
+});
