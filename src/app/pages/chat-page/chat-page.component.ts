f7a11a8afeeb8d364b3b5391481c99c020be8741
Finnick223 <filipf192@gmail.com>
2025-05-07 23:15:39 +0200

    feat:
    - add login page internationalization
    - add register page internationalization
    - add chat page internationalization
    - little fixes in register and login UI

--- src/app/pages/chat-page/chat-page.component.ts
+++ src/app/pages/chat-page/chat-page.component.ts
@@ -17,6 +17,7 @@ import { ChatService } from '@Services/chat/chat.service';
 import { ConversationMessageWithFlag } from '@Interfaces/conversation-message.interface';
 import { AuthTokenService } from '@Services/auth-token/auth-token.service';
 import { ActivatedRoute } from '@angular/router';
+import { TranslocoModule } from '@jsverse/transloco';
 @Component({
   selector: 'app-landing-page',
   imports: [
@@ -27,6 +28,7 @@ import { ActivatedRoute } from '@angular/router';
     InputTextModule,
     TooltipModule,
     ScrollPanelModule,
+    TranslocoModule,
   ],
   templateUrl: './chat-page.component.html',
   styleUrl: './chat-page.component.css',
