c381aa11164fc4a6189e32b29c179f2ba44937bc
Finnick223 <filipf192@gmail.com>
2025-05-07 23:20:05 +0200

    feat: add navbar translation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TranslocoModule } from '@jsverse/transloco';
 import { AuthTokenService } from '@Services/auth-token/auth-token.service';
 import { RoutingService } from '@Services/routing/routing.service';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [ButtonModule],
+  imports: [ButtonModule, TranslocoModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
